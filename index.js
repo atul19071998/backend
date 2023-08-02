25b42f876bb1aa7cffbce83dd0a24ac98922a903
Atul Choudhary <104958919+atul19071998@users.noreply.github.com>
2023-08-03 00:38:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 dotenv.config({path:'./config.env'});
 require('./db/conn');
